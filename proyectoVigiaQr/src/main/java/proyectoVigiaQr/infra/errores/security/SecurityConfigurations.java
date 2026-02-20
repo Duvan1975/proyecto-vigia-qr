c7251ef4b39f406807fbd109845e00147910c660
Duvan Ballesteros <ballexplorer1975@gmail.com>
2026-02-20 14:10:53 -0500

    Realizando configuraciones en SecurityConfigurations

--- proyectoVigiaQr/src/main/java/proyectoVigiaQr/infra/errores/security/SecurityConfigurations.java
+++ proyectoVigiaQr/src/main/java/proyectoVigiaQr/infra/errores/security/SecurityConfigurations.java
@@ -29,8 +29,11 @@ public class SecurityConfigurations {
         return http
                 .cors(cors -> cors.configurationSource(request -> {
                     var corsConfiguration = new CorsConfiguration();
-                    corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
-                    corsConfiguration.setAllowedOrigins(List.of("https://proyecto-vigia-qr.vercel.app"));
+                    corsConfiguration.setAllowedOrigins(List.of(
+                            "http://localhost:3000",
+                            "http://localhost:8080",
+                            "https://proyecto-vigia-qr.vercel.app"
+                    ));
                     corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
                     corsConfiguration.setAllowedHeaders(List.of("*"));
                     corsConfiguration.setAllowCredentials(true);
