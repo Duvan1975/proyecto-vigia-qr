a96c7cc04f64a2dd93f64d7380fb0e10db92996c
Duvan Ballesteros <ballexplorer1975@gmail.com>
2026-03-01 01:01:14 -0500

    Control auth in root

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,22 @@
-import ReactDOM from 'react-dom/client';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import { Menu } from './Menu';
 import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Menu />);
+import { Login } from "./Login";
+import { Menu } from "./Menu";
+
+function App() {
+  const [isAuth, setIsAuth] = useState(
+    !!localStorage.getItem("token")
+  );
+
+  return isAuth
+    ? <Menu />
+    : <Login onLoginSuccess={() => setIsAuth(true)} />;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
